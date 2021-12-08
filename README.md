34b1643c99857392a502de8a2c3d60ba84413e1d
ChazniK <Chazni.k9@gmail.com>
2021-12-08 19:09:26 +0200

    Done extracting odds component

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # odds-scraper
-Tool to scrape football odds from multiple betting sites
+Tool to scrape football odds from oddspedia
--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ with open('match.html', 'r') as html_file:
         match_header_content =  match.find('div', class_ = 'btools-match__header')
         match_sport_link = match_header_content.find('div', class_ = 'btools-match-sport').a
         match_sport = match_sport_link.get('title')
-        '''
+        
         print('----------------Header Section----------------------------')
         print(f'Sport: {match_sport}')
         match_sport_league = match_header_content.find('div', class_ = 'btools-match-league')
@@ -23,16 +23,22 @@ with open('match.html', 'r') as html_file:
         match_time = match_header_content.find('div', class_ = 'btools-match-time').span.text
         print(f'Match time: {match_time}')
         print('------------------------------------')
-        '''
+        
         print('----------------Body Section--------------------')
         match_body_contents = match.find('div', class_ = 'btools-match__body')
         match_body_competitors = match_body_contents.find('div', class_ = 'btools-match-teams')
-        #print(match_body_contents.prettify())
         competitor_home = match_body_competitors.a.contents[0].strip()
         competitor_away = match_body_competitors.a.contents[2].strip()
         print(f'Home competitor: {competitor_home}')
         print(f'Away competitor: {competitor_away}')
         match_bet_type = match_body_competitors.span.contents[0].strip()
-        print(f'Bet type: {match_bet_type}')
         match_odds = match_body_contents.find_all('div', class_ = 'btools-odd-mini')
-        print(match_odds)
+        for match_odd in match_odds:
+            match_odd_result = match_odd.find('div', class_ = 'btools-odd-mini__header').span.text
+            match_odd_value = match_odd.find('div', class_ = 'btools-odd-mini__value').span.text
+            match_odd_bookmaker_png = match_odd.find('div', class_ = 'btools-odd-mini__logo').img.get('src').split('/')[-1]
+            match_odd_bookmaker = match_odd_bookmaker_png.split('.')[0]
+            print(f'Match result: {match_odd_result}')
+            print(f'Bet odds: {match_odd_value}')
+            print(f'Bookmaker: {match_odd_bookmaker}')
+            print('------------------------------------')
